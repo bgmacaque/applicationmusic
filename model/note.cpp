6b477dd8a5a0ae09c76cb628a9412611912ac73d
Theo Gerriet <theo.gerriet8@etu.univ-lorraine.fr>
2013-12-11 21:28:16 +0100

    Added comments on the Note.cpp class to test commit

--- model/note.cpp
+++ model/note.cpp
@@ -1,26 +1,46 @@
 #include "note.h"
 
+/*
+	This class represents the notes that will compose a tab/
+	Notes can be grouped to form a Chord.
+*/
+
+
+//*************
+// Constructor
+//*************
+
 Note::Note(std::string name, int frequency){
     this->m_name = name;
     this->m_frequency = frequency;
 }
 
+//**********
+// Methods
+//**********
+
+/* @return the frequency of the note*/
+
 int Note::getFrequency() const{
     return this->m_frequency;
 }
 
+/* @void set the frenquency to the 'frequency' value*/
 void Note::setFrequency(int frequency){
     this->m_frequency = frequency;
 }
 
+/* @return the name of the note*/
 std::string Note::getName() const{
     return this->m_name;
 }
 
+/* @void set the name of the not to the 'name' value*/
 void Note::setName(std::string name){
     this->m_name = name;
 }
 
+/* @void play a note with a volume value equals to 'volume'*/
 void Note::play(double volume){
 
 }
