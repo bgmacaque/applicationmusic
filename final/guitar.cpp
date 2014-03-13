a1df60ed2846d7e858ddafa07e6b5e09da3983d1
Theo Gerriet <theo.gerriet8@etu.univ-lorraine.fr>
2014-03-13 16:02:57 +0100

    Added method get_frets() in class Guitar.cpp

--- final/guitar.cpp
+++ final/guitar.cpp
@@ -18,6 +18,17 @@ void Guitar::kill(){
     delete m_instance;
 }
 
+*Fret[]::get_frets(*chord chord){
+    int i;
+    int j=0;
+    *Fret[] frets=new *Fret[chord.length];
+    for (i=0;i<chord.length;i++){
+        for (j=0;j<6;j++){
+            if (m_instance.strings[j].playable) frets[i]=m_instance.strings[j].get_fret();
+        }
+    }
+}
+
 Guitar *Guitar::get_instance(){
     if(m_instance == NULL){
         m_instance = new Guitar();
